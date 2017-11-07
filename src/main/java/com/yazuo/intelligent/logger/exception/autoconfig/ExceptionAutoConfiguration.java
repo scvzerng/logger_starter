4048a93e4925e26ec8ef14c86d75e077557ae870
scvzerng <443683059a>
2017-11-07 09:55:05 +0800

    类路径错误

--- src/main/java/com/yazuo/intelligent/logger/exception/autoconfig/ExceptionAutoConfiguration.java
+++ src/main/java/com/yazuo/intelligent/logger/exception/autoconfig/ExceptionAutoConfiguration.java
@@ -1,7 +1,7 @@
 package com.yazuo.intelligent.logger.exception.autoconfig;
 
-import com.yazuo.intelligent.exception.builder.ErrorResultBuilder;
-import com.yazuo.intelligent.exception.builder.RestErrorResultBuilder;
+import com.yazuo.intelligent.logger.exception.builder.ErrorResultBuilder;
+import com.yazuo.intelligent.logger.exception.builder.RestErrorResultBuilder;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
