79b95c5b807dc51d4593efb88ddbc5e0616d1625
scvzerng <443683059a>
2017-11-07 13:49:14 +0800

    移除 application.yml

--- src/main/java/com/yazuo/intelligent/logger/exception/autoconfig/ExceptionAutoConfiguration.java
+++ src/main/java/com/yazuo/intelligent/logger/exception/autoconfig/ExceptionAutoConfiguration.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
 import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import javax.servlet.Servlet;
@@ -14,6 +15,7 @@ import javax.servlet.Servlet;
 @Configuration
 @ConditionalOnWebApplication
 @ConditionalOnClass(value = {Servlet.class, RestControllerAdvice.class})
+@PropertySource("logger.yml")
 public class ExceptionAutoConfiguration {
     @Bean
     @ConditionalOnMissingBean
