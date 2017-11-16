75cb5d21bb03d697f590bc877899a052712f6e7f
scvzerng <443683059a>
2017-11-16 15:02:29 +0800

    修复ExceptionAdvice 参数缺失的问题

--- src/main/java/com/yazuo/intelligent/exception/mvc/ExceptionAdvice.java
+++ src/main/java/com/yazuo/intelligent/exception/mvc/ExceptionAdvice.java
@@ -3,10 +3,12 @@ package com.yazuo.intelligent.exception.mvc;
 import com.yazuo.intelligent.exception.BusinessException;
 import com.yazuo.intelligent.exception.builder.ResultBuilder;
 import com.yazuo.intelligent.exception.response.ErrorResult;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.cloud.sleuth.Tracer;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.HandlerMethod;
 
 import javax.annotation.Resource;
 
@@ -18,12 +20,12 @@ public class ExceptionAdvice  {
     private ResultBuilder resultBuilder;
 
     @ExceptionHandler(BusinessException.class)
-    public ErrorResult processRestError(BusinessException exception){
-        return resultBuilder.buildError(exception,tracer);
+    public ErrorResult processRestError(BusinessException exception, HandlerMethod method){
+        return resultBuilder.buildError(method.getMethodAnnotation(ApiOperation.class),exception,tracer);
     }
     @ExceptionHandler(Exception.class)
-    public ErrorResult processSysError(Exception e){
-        return resultBuilder.buildError(e,tracer);
+    public ErrorResult processSysError(Exception e,HandlerMethod method){
+        return resultBuilder.buildError(method.getMethodAnnotation(ApiOperation.class),e,tracer);
     }
 
 
