5246bda6269b5b1a684e585d9288e57b0d7f555f
scvzerng <443683059a>
2017-11-16 09:52:13 +0800

    业务异常默认code码定义为2000

--- src/main/java/com/yazuo/intelligent/logger/exception/BusinessException.java
+++ src/main/java/com/yazuo/intelligent/logger/exception/BusinessException.java
@@ -4,4 +4,12 @@ public class BusinessException extends AbstractException {
     public BusinessException(int code, String message, Throwable e) {
         super(code, message, e);
     }
+
+    public BusinessException( String message, Throwable e) {
+        this(2000,message,e);
+    }
+
+    public BusinessException(String message) {
+       this(2000,message,null);
+    }
 }
