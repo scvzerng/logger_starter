a01e2b0b608d53578161495775bdce4a1ae5c800
scvzerng <443683059a>
2017-11-16 15:07:13 +0800

    优化LoggerAutoConfiguration.java的实例化时机

--- src/main/java/com/yazuo/intelligent/autoconfig/LoggerAutoConfiguration.java
+++ src/main/java/com/yazuo/intelligent/autoconfig/LoggerAutoConfiguration.java
@@ -4,6 +4,7 @@ import com.alibaba.fastjson.JSON;
 import com.yazuo.intelligent.logger.*;
 import com.yazuo.intelligent.logger.filter.HttpObjectFilter;
 import com.yazuo.intelligent.logger.filter.LoggerParamsFilter;
+import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
@@ -12,7 +13,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ConditionalOnClass({Logger.class, JSON.class})
+@ConditionalOnClass({Logger.class, JSON.class, ApiOperation.class})
 @Slf4j
 public class LoggerAutoConfiguration {
     @Bean
