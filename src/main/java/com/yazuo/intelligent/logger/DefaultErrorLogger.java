8193131fc9d54637f195e195f32d59c21a5bd584
scvzerng <443683059a>
2017-12-27 11:53:18 +0800

    fixed 类不存在

--- src/main/java/com/yazuo/intelligent/logger/DefaultErrorLogger.java
+++ src/main/java/com/yazuo/intelligent/logger/DefaultErrorLogger.java
@@ -1,8 +1,8 @@
 package com.yazuo.intelligent.logger;
 
 import com.alibaba.fastjson.JSON;
-import com.yazuo.intelligent.exception.AbstractException;
-import com.yazuo.intelligent.exception.utils.ExceptionUtils;
+import com.yazuo.intelligent.common.exception.AbstractException;
+import com.yazuo.intelligent.common.exception.utils.ExceptionUtils;
 import com.yazuo.intelligent.logger.filter.LoggerParamsFilter;
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.reflect.MethodSignature;
