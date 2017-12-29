8042e9c53f9f0754ad3087fc169172ba4bb8e4ae
scvzerng <443683059a>
2017-12-29 15:23:59 +0800

    异常处理增强 参数校验给出方法参数名

--- src/main/java/com/yazuo/intelligent/autoconfig/ExceptionAutoConfiguration.java
+++ src/main/java/com/yazuo/intelligent/autoconfig/ExceptionAutoConfiguration.java
@@ -2,11 +2,13 @@ package com.yazuo.intelligent.autoconfig;
 
 import com.yazuo.intelligent.exception.builder.RestResultBuilder;
 import com.yazuo.intelligent.exception.builder.ResultBuilder;
+import com.yazuo.intelligent.validation.ParameterNameValidationMethodValidationPostProcessor;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.validation.beanvalidation.MethodValidationPostProcessor;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -24,6 +26,6 @@ public class ExceptionAutoConfiguration {
     }
     @Bean
     public MethodValidationPostProcessor methodValidationPostProcessor(){
-        return new MethodValidationPostProcessor();
+        return new ParameterNameValidationMethodValidationPostProcessor(Validated.class);
     }
 }
--- src/main/java/com/yazuo/intelligent/exception/mvc/ExceptionAdvice.java
+++ src/main/java/com/yazuo/intelligent/exception/mvc/ExceptionAdvice.java
@@ -5,6 +5,8 @@ import com.yazuo.intelligent.common.response.GenericResponse;
 import com.yazuo.intelligent.exception.builder.ResultBuilder;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.cloud.sleuth.Tracer;
+import org.springframework.validation.BindException;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.method.HandlerMethod;
 
 import javax.annotation.Resource;
+import javax.validation.ConstraintViolationException;
 
 import static java.util.stream.Collectors.toList;
 import static org.springframework.http.HttpStatus.BAD_REQUEST;
@@ -31,23 +34,52 @@ public class ExceptionAdvice  {
     public GenericResponse<?> processSysError(Exception e,HandlerMethod method){
         return resultBuilder.buildError(method.getMethodAnnotation(ApiOperation.class),e,tracer);
     }
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public GenericResponse<Object> processValidateException(MethodArgumentNotValidException e){
+    @ExceptionHandler({
+            MethodArgumentNotValidException.class,
+            ConstraintViolationException.class,
+            BindException.class
+           })
+    public GenericResponse<Object> processValidateException(Exception e){
         GenericResponse<Object> response = new GenericResponse<>();
         response.setCode(BAD_REQUEST.value());
         response.setMessage("参数校验失败");
-        response.setDetails(
-                e.getBindingResult().
-                        getFieldErrors()
-                        .stream()
-                        .map(fieldError ->
-                                new GenericResponse.FieldError(
-                                        fieldError.getField(),
-                                        fieldError.getDefaultMessage(),
-                                        fieldError.getRejectedValue()
-                                        )
+        if(e instanceof MethodArgumentNotValidException||e instanceof BindException){
+            BindingResult bindingResult = null;
+            if(e instanceof MethodArgumentNotValidException){
+                bindingResult = ((MethodArgumentNotValidException) e).getBindingResult();
+            }
 
-                        ).collect(toList()));
+            if(e instanceof BindException){
+                bindingResult = ((BindException) e).getBindingResult();
+            }
+            response.setDetails(
+                    bindingResult.
+                            getFieldErrors()
+                            .stream()
+                            .map(fieldError ->
+                                    new GenericResponse.FieldError(
+                                            fieldError.getField(),
+                                            fieldError.getDefaultMessage(),
+                                            fieldError.getRejectedValue()
+                                    )
+
+                            ).collect(toList()));
+        }
+
+        if(e instanceof ConstraintViolationException){
+            ConstraintViolationException violationException = (ConstraintViolationException) e;
+            response.setDetails( violationException.getConstraintViolations()
+                    .stream()
+                    .map(constraintViolation ->
+                            new GenericResponse.FieldError(
+                                    constraintViolation.getPropertyPath().toString(),
+                                    constraintViolation.getMessage(),
+                                    constraintViolation.getInvalidValue()
+                            ))
+                    .collect(toList()));
+
+        }
+
         return response;
     }
 
